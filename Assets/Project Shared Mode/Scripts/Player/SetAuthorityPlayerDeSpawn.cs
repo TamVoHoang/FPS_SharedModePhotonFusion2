8738744cb00bd011be68b3699f2b2284a1db93e1
TamVoHoang <130343021+TamVoHoang@users.noreply.github.com>
2025-01-05 03:21:43 +0700

    Feature: change master client
    - before change GameManagerUIHandler script to help count down cotinue

--- Assets/Project Shared Mode/Scripts/Player/SetAuthorityPlayerDeSpawn.cs
+++ Assets/Project Shared Mode/Scripts/Player/SetAuthorityPlayerDeSpawn.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Fusion;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SetAuthorityPlayerDeSpawn : NetworkBehaviour
 {
+    [Networked]
+    PlayerRef playerRef{get; set;}
     public override void Spawned()
     {
         base.Spawned();
@@ -17,12 +20,30 @@ public class SetAuthorityPlayerDeSpawn : NetworkBehaviour
 
         if(SceneManager.GetActiveScene().name == "Ready") {
             SetReadyUIhandlerStateAuthority();
+
+            if(runner.IsSharedModeMasterClient) {
+                Debug.Log($"_____ master client just left room = " + runner.LocalPlayer);
+                PlayerRef playerRef = FindNextMasterClient(runner);
+                if(playerRef == PlayerRef.None) return;
+                Debug.Log($"_____master client next = " + playerRef);
+                runner.SetMasterClient(playerRef);
+            }
+            
         } else {
+            if(runner.IsSharedModeMasterClient) {
+                if(Object.HasStateAuthority) {
+                    Debug.Log($"_____ master client just left room = " + runner.LocalPlayer);
+                    PlayerRef playerRef = FindNextMasterClient(runner);
+                    Debug.Log($"_____master client next = " + playerRef);
+                    if(playerRef == PlayerRef.None) return;
+                    runner.SetMasterClient(playerRef);
+                }
+                    
+            }
             // trong scene battle
             SetWeaponPickupStateAuthority();
             SetGameMangerStateAuthority();
         }
-        
     }
 
     void SetWeaponPickupStateAuthority() {
@@ -65,6 +86,27 @@ public class SetAuthorityPlayerDeSpawn : NetworkBehaviour
         {
             throw;
         }
-        
     }
+
+
+    // tim new master client != this despawn
+    private PlayerRef FindNextMasterClient(NetworkRunner runner)
+    {
+        if (runner == null) return PlayerRef.None;
+
+        // Get all active players except the current one
+        var players = runner.ActivePlayers
+            .Where(p => p != runner.LocalPlayer)
+            .OrderBy(p => p.RawEncoded)
+            .ToList();
+
+        return players.Any() ? players.First() : PlayerRef.None;
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    void RPC_RequestMasterClientChange(PlayerRef playerRef) {
+        this.playerRef =  playerRef;
+    }
+
+
 }
